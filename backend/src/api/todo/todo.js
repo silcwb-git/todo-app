848bc79c8eff0dd53e5038d37468b21c5cc55bfe
Silvana Siqueira <silvana.siqueira@gmail.com>
2020-03-28 13:42:58 -0300

    refactoring ssimples do código

--- backend/src/api/todo/todo.js
+++ backend/src/api/todo/todo.js
@@ -3,8 +3,8 @@ const mongoose = restful.mongoose
 
 const todoSchema = new mongoose.Schema({
     description: { type: String, require:true },
-    done: { type: Boolean, require: true, defautl: false },
-    createdAt: { type: Date, defautl: Date.now }
+    done: { type: Boolean, require: true, default: false },
+    createdAt: { type: Date, default: Date.now }
 })
 
 module.exports = restful.model('Todo', todoSchema)
