9a6043b603e8d3364861528bb040033a3cbaec79
Silvana Siqueira <silvana.siqueira@gmail.com>
2020-03-28 22:01:33 -0300

    criados os arquivos app e index.jsx

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,11 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+export default props => (
+    <div className='container'>
+        <hi>Teste</hi>
+    </div>
+)
+  
